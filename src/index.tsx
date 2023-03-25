248d286af764b9eb29876e3157361b811a4fe173
Louise Connell <98759562+Connell-L@users.noreply.github.com>
2023-03-25 10:19:41 +0000

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return (
+        <div>
+            <h1>Hello World!</h1>
+        </div>
+    );
+};
+
+const root = document.getElementById('root');
+ReactDOM.render(<App />, root);
